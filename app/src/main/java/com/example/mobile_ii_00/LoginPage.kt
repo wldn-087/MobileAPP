f3f5d452feccbcae2d39012376e2eaaaef5df745
Wildan <wildanfahmi087@gmail.com>
2024-12-01 20:08:48 +0700

    Login

--- app/src/main/java/com/example/mobile_ii_00/LoginPage.kt
+++ app/src/main/java/com/example/mobile_ii_00/LoginPage.kt
@@ -1,11 +1,35 @@
 package com.example.mobile_ii_00
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 
 @Preview
 @Composable
 fun LoginPage(){
-    Text(text = "Login to your Account")
+    //Backgroud Container
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(horizontal = 24.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.logo_green_with_icon),
+            contentDescription = "App Logo",
+            modifier = Modifier
+                .size(120.dp)
+                .padding(bottom = 16.dp)
+        )
+    }
 }
