feb24903be5b5134c75b53f83ea0a401d04781bb
Wildan <wildanfahmi087@gmail.com>
2024-12-01 19:23:05 +0700

    Add Resources

--- app/src/main/java/com/example/mobile_ii_00/LoginPage.kt
+++ app/src/main/java/com/example/mobile_ii_00/LoginPage.kt
@@ -1,10 +1,11 @@
 package com.example.mobile_ii_00
 
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.tooling.preview.Preview
 
 @Preview
 @Composable
 fun LoginPage(){
-
+    Text(text = "Login to your Account")
 }
